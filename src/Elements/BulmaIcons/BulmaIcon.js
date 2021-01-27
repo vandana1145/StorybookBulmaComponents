945e88f94da3a9adedf9e84ce0e18224620ed44b
Vandana Kumari <vandana.kumari@semapp.de>
2021-01-27 12:38:11 +0100

    Hardcoded the icon size inside the icon element

--- src/Elements/BulmaIcons/BulmaIcon.js
+++ src/Elements/BulmaIcons/BulmaIcon.js
@@ -2,11 +2,37 @@ import React from 'react';
 import { FaApple } from 'react-icons/fa';
 
 const BulmaIcon = (props) => {
-    const { variant="icon", className, children, color, size, ...rest} = props
+    const { variant="icon", className, children, color, size, text, ...rest} = props
+
+    /*const renderIcon = () => {
+        return (
+            <span class="icon">
+                <i class="fas FaApple"></i>
+            </span>
+        );
+    }
+
+    const renderIconWithText = () => {
+        return (
+            <span class="icon-text">
+                <span class="icon">
+                    <i class="fas FaApple"></i>
+                </span>
+                <span>Home</span>
+            </span>
+        );
+    }
+
+    if ( text ) {
+        return renderIconWithText();
+    } else {
+        return renderIcon();
+    }*/
+
     return(
         <div>
         <span className={`icon ${variant}`} {...rest}>
-            {<FaApple/>}
+            {<FaApple size="2rem" />}
         </span>
         </div>
     );
